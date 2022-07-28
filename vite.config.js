564143eb48b439e1186e38b357f066b0a55fb1cc
Jessica Sachs <jess@jessicasachs.io>
2022-07-28 11:48:37 -0400

    npm run test:unit failing in vitest

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue2 from '@vitejs/plugin-vue2'
+import vue2Jsx from '@vitejs/plugin-vue2-jsx'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue2(), vue2Jsx()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
